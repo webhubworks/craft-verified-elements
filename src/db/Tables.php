c6e1c5d137cde8cfadda659d646c44343586019d
Ryan <white@webhub.de>
2026-05-21 16:27:58 +0200

    feat: added Table abstract class for table related constants

---
+++ src/db/Tables.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace webhubworks\verifiedentries\db;
+
+abstract class Tables
+{
+    const ENTRIES = '{{%verifiedentries_entryattributes}}';
+    const SECTIONS = '{{%verifiedentries_sections}}';
+}
