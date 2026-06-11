d6a52038364132cd8c146f7fa74a6f5067ae3854
Ryan <white@webhub.de>
2026-06-11 15:45:35 +0200

    feat: adding a guard to plugin's Pest.php file to ensure a test database is instantiated

--- tests/Pest.php
+++ tests/Pest.php
@@ -2,6 +2,14 @@
 
 use markhuot\craftpest\test\RefreshesDatabase;
 use markhuot\craftpest\test\TestCase;
+use craft\helpers\App;
+
+
+$databaseName = App::env('CRAFT_DB_DATABASE');
+if (! str_contains((string)$databaseName, 'test')) {
+    fwrite(STDERR, "Aborting: Pest must run against a dedicated test database. Run via `ddev composer test:verified-entries` or set CRAFT_DB_DATABASE to a database whose name contains 'test'.\n");
+    exit(1);
+}
 
 /*
 |---------------------------------------------------------------------------------------------------
